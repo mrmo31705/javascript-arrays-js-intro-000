bbc7d7b5140f635bfe90a94497b7137e22482bd0
Maurice Davis <mrmo85@yahoo.com>
2018-07-11 13:41:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,3 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
 
-function addElementToBeginningOfArray("cat", "dog"); {
-  addElementToBeginningOfArray.push("mouse");
-}
-
-function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
-}
