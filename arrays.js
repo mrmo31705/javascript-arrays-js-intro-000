d3f79676d79dec19532b27a8a4255a74929ce98e
Maurice Davis <mrmo85@yahoo.com>
2018-07-11 13:29:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function destructivelyAppendKitten(name) {
-  kittens.push(name);
-  return kittens;
+function addElementToBeginningOfArray("cat", "dog"); {
+  addElementToBeginningOfArray.push("mouse");
 }
